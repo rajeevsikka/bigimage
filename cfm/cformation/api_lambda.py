480c1d157d1d078d998438da60794f17c1bc8b08
Powell Quiring <pquiring@us.ibm.com>
2017-01-04 16:11:49 -0800

    api gateway working, codepipeline adjusted in preparation for cf

--- cfm/cformation/api_lambda.py
+++ cfm/cformation/api_lambda.py
@@ -48,24 +48,73 @@ def template(stackName='bigimage'):
 
     # TODO should the code be in here?
     codeString='''
-from __future__ import print_function
-
 import json
+import os
+import urllib2
 
 print('Loading elasticsearch keyword function')
 
+#ELASTICSEARCH_URL = "https://search-bigimage-2gtgp2nq3ztfednwgnd6ma626i.us-west-2.es.amazonaws.com"
+ELASTICSEARCH_URL = os.environ['ELASTICSEARCH_URL']
+
+def extractImages(tweet):
+    'return a list of some of the images in a tweet'
+    ret = []
+    for media in tweet.get('_source', {}).get('entities', {}).get('media', {}):
+        for key in ['media_url', 'media_url_https']:
+            if key in media:
+                ret.append(media[key])
+    return ret
+
+def extractVideos(tweet):
+    'return a list of videos of the images in a tweet'
+    ret = []
+    for media in tweet.get('_source', {}).get('extended_entities', {}).get('media', {}):
+        for variant in media.get('video_info', {}).get('variants', {}):
+            ret.append(variant)
+    return ret
+
+
+def condensedTweets(word):
+    urlString = ELASTICSEARCH_URL + '/indexname/_search'
+    data = {
+        "query": {
+            "query_string" : {
+                "query":word,
+                "analyze_wildcard":True
+            }
+        }
+    }
+    request = urllib2.Request(urlString, json.dumps(data))
+    f = urllib2.urlopen(request)
+    resultString = f.read()
+
+    result = json.loads(resultString)
+    ret=[]
+    for tweet in result['hits']['hits']:
+        tweetEntry = {}
+        tweetEntry['text'] = tweet['_source']['text']
+        id = tweet['_source']['id']
+        tweetEntry['url'] = 'https://twitter.com/_/status/' + str(id)
+        tweetEntry['videos'] = extractVideos(tweet)
+        tweetEntry['images'] = extractImages(tweet)
+        ret.append(tweetEntry)
+    return ret
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     context_vars = dict(vars(context))
     keys = list(context_vars.keys())
+    word = event.get('params', {}).get('querystring', {}).get('word', 'coffee')
+    tweets = condensedTweets(word)
     for key in keys:
         try:
             json.dumps(context_vars[key])
         except:
             print('delete:', key)
             context_vars.pop(key)
-    ret = {'ret':'abcd', 'version': '2.1', '~event': event, '~context': context_vars}
+    
+    ret = {'ret':tweets, 'version': '2.2', '~environ': dict(os.environ), '~event': event, '~context': context_vars}
     return ret
 
 '''
